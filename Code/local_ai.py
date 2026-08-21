c809c15528f8ec70bf85ca50399cb607e7561546
Fadrina Denoth <denof1@bfh.ch>
2026-08-21 23:24:34 +0200

    back up

---
+++ Code/local_ai.py
@@ -0,0 +1,24 @@
+import requests
+import os
+
+
+def local_ai(document1: str) -> list[str]:
+    STONEY_KEY = os.getenv("STONEY_KEY")
+    MODEL = 'apertus-ai/Apertus-v1.5-8B'
+    PROMPT = "hello"
+    MAX_TOKENS = 100
+
+    url = 'https://llm.stoney-cloud.com/v1/chat/completions'
+    headers = {"Authorization: Bearer $STONEY_KEY", "Content-Type: application/json"}
+
+    payload = {
+        "model": MODEL,
+        "message": [{"role": "user", "content": PROMPT}],
+        "max_tokens": MAX_TOKENS,
+
+    }
+
+    response = requests.request("POST", url, headers=headers, json=payload)
+    response.raise_for_status()
+    return response.json()["completions"]
+
